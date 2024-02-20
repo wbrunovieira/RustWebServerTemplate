f2896e0a5899fc7ba0b508bb5c3c6657fd6dae94
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-20 12:39:55 -0300

    added structures

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-
-
-
 use actix_cors::Cors;
-use actix_web::{http::header, web, App, HttpServer, HttpResponse, Responder};
-use serde::{Deserialize, Serialize};
-use reqwest::Client as HttpClient;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
 use async_trait::async_trait;
-use std::sync::Mutex;
+use reqwest::Client as HttpClient;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    task: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
 
 fn main() {
     println!("Hello, world!");
