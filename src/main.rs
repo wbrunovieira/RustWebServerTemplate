97b2c6f91d7b4d4287186a8099d80547bfad1413
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-20 12:33:03 -0300

    added packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+
+
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpServer, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use reqwest::Client as HttpClient;
+use async_trait::async_trait;
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+
 fn main() {
     println!("Hello, world!");
 }
