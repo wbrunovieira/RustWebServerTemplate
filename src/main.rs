6ac1de2156bc087bc5d1b234d5deae61e06df981
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-21 16:38:21 -0300

    implement Database

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,56 @@ struct Database {
     users: HashMap<u64, User>,
 }
 
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(k: task.id, v: task);
+    }
+    fn get(&self, id: &u64) -> Option<&Task> { 
+        self.tasks.get(id);
+    }
+    fn get_all(&self) -> Vec<&Task> { 
+        self.tasks.values().collect();
+    }
+    fn delete(&self, id: &u64) { 
+        self.tasks.remove(id);
+    }
+
+    fn update(&mut self, task: Task) {
+        self.tasks.insert(k: task.id, v: task);
+    }
+    
+    // USER DATA RELATED FUNCTIONS
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user_by_name(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|u| u.username == username)
+    }
+
+    // DATABASE SAVING
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let data: String = serde_json::to_string(&self)?;
+        let mut file: fs::File = fs::File::create("database.json")?;
+        file.write_all(data.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file_content: String = fs::read_to_string("database.json")?;
+        let db: Database = serde_json::from_str(&file_content)?;
+        Ok(db)
+    }
+
+} impl Database
+
 fn main() {
     println!("Hello, world!");
 }
